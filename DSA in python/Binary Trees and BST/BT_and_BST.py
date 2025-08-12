bb64d5de01e4a9bcca29aa4283e4b5b0e7183ab3
Claudio Gonzalez <claudio.glez24@gmail.com>
2025-08-11 20:42:07 -0600

    bst and heaps

--- DSA in python/Binary Trees and BST/BT_and_BST.py
+++ DSA in python/Binary Trees and BST/BT_and_BST.py
@@ -43,7 +43,40 @@ def pre_order(node): #If the node is None
 
 
 #Iterative pre order Traversal (DFS) Time complexity: O(n)
-    def pre_order_iterative(node):
-        stack = [node] #Initialize with the root node
-        while stack: #while we have something in the stack
-            node = stk.pop() # we process the node that is in the top of the stack
+def pre_order_iterative(node):
+    stack = [node] #Initialize with the root node
+    while stack: #while we have something in the stack
+        node = stack.pop() # we process the node that is in the top of the stack
+        print(node)
+        if node.right:stack.append(node.right)
+        if node.left: stack.append(node.left)
+        # we do right then left so that the left node ends on top of the stack, 
+        # because we need to go down the left side first
+
+pre_order_iterative(A)
+
+# Level order traversla (BFS) Time complexity: O(n) space: O(n)
+
+from collections import deque
+
+def level_order(node):
+    q = deque()
+    q.append(node)
+
+    while q:
+        node = q.popleft()
+        print(node)
+        if node.left: q.append(node.left)
+        if node.right: q.append(node.right)
+level_order(A)
+
+
+#Check if a value exists (DFS) Time: O(n)
+def search (node,target):
+    if not node:
+        return False # we did not find the target
+    if node.val  == target:
+        return True
+    return search(node.left,target) or search(node.right,target)
+
+search(A,4)
---
+++ DSA in python/Heaps and priority queues/Heaps_and_priority_queues.py
@@ -0,0 +1,10 @@
+#Build a MinHEAP (heapify)
+#Time O(n), Space O(n)
+
+A = [-4,3,1,0,2,5,10,8,12,9]
+
+import heapq
+
+heapq.heapify(A)
+
+print(A)
